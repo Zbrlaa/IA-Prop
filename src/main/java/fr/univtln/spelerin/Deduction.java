57b1e5586add5973f6d730c5864f0d551de15ca6
Zbrlaa <shawn.pelerin@gmail.com>
2025-09-29 23:57:17 +0200

    Ajout déroulement/logique de isProved

--- src/main/java/fr/univtln/spelerin/Deduction.java
+++ src/main/java/fr/univtln/spelerin/Deduction.java
@@ -31,15 +31,34 @@ public class Deduction{
 	public boolean isProved(){
 		Formule f = this.toFormule();
 		Node root = Node.ofFormule(f);
-		
-		Set<Node> nodesToTreat = Set.of(root);
-		boolean verif = true;
-		while(verif){
-			for(Node n : nodesToTreat){
+
+		Set<Node> nodesToAffect = Set.of(root); //Noeuds qu'on étend (dont on affecte les enfants)
+		Set<Node> nodesToTreat = Set.of(root); //Noeuds qu'on traite (dont on regarde la formule) ! Faudrait un ordre : File"
+		boolean toDo = true; //Pour gerer la terminaison
+		boolean result; //Pour le resultat de l'algo
+		//Tant qu'on a des noeufs à traiter ou qu'une branche reste ouverte
+		while(toDo){
+			//Faire des verifs là :
+
+			//Pour tout les noeuds à traiter
+			for(Node nTT : nodesToTreat){
+				//Pour toutes les formules du noeud
+				for(Formule form : nTT.getFormules()){
+					//Creer methode sur formule qui renvoies les noeuds enfants que ça crée : Set<Node> childs = f.toChildNodes()
+					//Pour tout les noeuds toAffect
+					for(Node nTA : nodesToAffect){
+						//On étend le noeud na.extend(childs) : ajoute les enfants, étend allFormules et change formules
+						//Verif des enfants :
+							//Si noeud fini et pas close -> on fini tout, toDo et result false
+							//Si noeud fermé -> on l'oublis juste
+							//Sinon, il devient a affecté et traité
+						
+						//Important : voir si on étend bien les branches où on est et pas les voisines !!
+					}
 				
+				}
 			}
 		}
-
 		return false;
 	}
 }
